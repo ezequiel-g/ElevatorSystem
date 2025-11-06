d623ccc1e91724f2537b5ddab9d3f58069ac7234
ezequiel-g <ezequielghione@gmail.com>
2025-11-06 18:16:24 -0300

    integration test 1

--- src/test/java/com/challenge/kindalab/ElevatorSystem/ApplicationTests.java
+++ src/test/java/com/challenge/kindalab/ElevatorSystem/ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.challenge.kindalab.ElevatorSystem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void getElevators_shouldReturnAList() {
+        ResponseEntity<List> response = restTemplate.getForEntity("/buildings", List.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
