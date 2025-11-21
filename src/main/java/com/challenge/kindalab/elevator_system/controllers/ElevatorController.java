0b71cc4af452f0e4ad26dc1ccd29a3264ffb085e
ezequiel-g <ezequielghione@gmail.com>
2025-11-21 02:12:06 -0300

    using better exception and adding exception check on mocked tests.

--- src/main/java/com/challenge/kindalab/elevator_system/controllers/ElevatorController.java
+++ src/main/java/com/challenge/kindalab/elevator_system/controllers/ElevatorController.java
@@ -5,8 +5,10 @@ import com.challenge.kindalab.elevator_system.domain.Elevator;
 import com.challenge.kindalab.elevator_system.domain.Floor;
 import com.challenge.kindalab.elevator_system.handlers.ElevatorRequestHandler;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Objects;
@@ -18,8 +20,8 @@ import java.util.UUID;
 @RestController
 public class ElevatorController {
 
-    private ElevatorRequestHandler elevatorRequestHandler;
-    private Building building;
+    private final ElevatorRequestHandler elevatorRequestHandler;
+    private final Building building;
 
     @GetMapping("")
     ResponseEntity<List<Elevator>> getElevators() {
@@ -28,25 +30,37 @@ public class ElevatorController {
 
     @PutMapping("{id}/call/{floorNumber}")
     ResponseEntity<Elevator> call(@PathVariable int id, @PathVariable int floorNumber) {
-        Elevator elevator = building.getElevators().get(id);
-        Objects.requireNonNull(elevator, "please check the provided elevator id: " + id);
-        Floor floor = building.getFloors().get(floorNumber);
-        Objects.requireNonNull(floor, "please check the provided floorNumber: " + floorNumber);
+        Elevator elevator = getElevator(id);
+        Floor floor = getFloor(floorNumber);
 
         elevatorRequestHandler.call(elevator, floor);
 
         return ResponseEntity.ok(elevator);
     }
 
+    private Floor getFloor(int floorNumber) {
+        Floor floor = building.getFloors().get(floorNumber);
+        if (Objects.isNull(floor)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Floor not found, please check provided floorNumber: " + floorNumber);
+        }
+        return floor;
+    }
+
+    private Elevator getElevator(int id) {
+        try {
+            return building.getElevators().get(id);
+        } catch (RuntimeException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Elevator not found, please check provided id: " + id);
+        }
+    }
+
     @PutMapping("{id}/load/{floorNumber}")
     ResponseEntity<Elevator> load(@PathVariable int id, @PathVariable int floorNumber, @RequestParam int weight) {
-        Elevator elevator = building.getElevators().get(id);
-        Objects.requireNonNull(elevator, "please check the provided elevator id: " + id);
-        Floor floor = building.getFloors().get(floorNumber);
-        Objects.requireNonNull(floor, "please check the provided floorNumber: " + floorNumber);
+        Elevator elevator = getElevator(id);
+        getFloor(floorNumber);
 
         if (elevator.getCabin().getFloorNumber() != floorNumber) {
-            throw new RuntimeException("do nothing, Elevator Cabin isn't on this floor yet.");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Elevator Cabin isn't on this floor yet, please use floorNumber: " + elevator.getCabin().getFloorNumber());
         }
 
         elevatorRequestHandler.load(elevator, weight);
@@ -56,10 +70,10 @@ public class ElevatorController {
 
     @PutMapping("{id}/move")
     ResponseEntity<Elevator> move(@PathVariable int id) {
-        Elevator elevator = building.getElevators().get(id);
-        Objects.requireNonNull(elevator, "please check the provided elevator id: " + id);
+        Elevator elevator = getElevator(id);
+
         if (elevator.getElevatorRequests().isEmpty()) {
-            throw new RuntimeException("do nothing, there are not pending requests.");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "There are not pending requests for Elevator, you can add request by using elevator call or elevator destination APIs");
         }
 
         elevatorRequestHandler.move(elevator);
@@ -69,10 +83,8 @@ public class ElevatorController {
 
     @PutMapping("{id}/destination/{floorNumber}")
     ResponseEntity<Elevator> destination(@PathVariable int id, @PathVariable int floorNumber, @RequestParam Optional<UUID> keycard) {
-        Elevator elevator = building.getElevators().get(id);
-        Objects.requireNonNull(elevator, "please check the provided elevator id: " + id);
-        Floor floor = building.getFloors().get(floorNumber);
-        Objects.requireNonNull(floor, "please check the provided floorNumber: " + floorNumber);
+        Elevator elevator = getElevator(id);
+        Floor floor = getFloor(floorNumber);
 
         if (Elevator.Type.FREIGHT.equals(elevator.getType())) {
             elevatorRequestHandler.call(elevator, floor);
@@ -80,11 +92,11 @@ public class ElevatorController {
             if (Objects.isNull(floor.getKeycard())) {
                 elevatorRequestHandler.call(elevator, floor);
             } else if (keycard.isEmpty()) {
-                throw new RuntimeException("keycard is required to reach the floor: " + floor.getFloorNumber());
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Keycard is required to reach the floor: " + floor.getFloorNumber());
             } else if (UUID.fromString(floor.getKeycard()).equals(keycard.get())) {
                 elevatorRequestHandler.call(elevator, floor);
             } else {
-                throw new RuntimeException("provided keycard does match, please check it: " + keycard.get());
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Provided keycard does match, please check keycard for floorNumber: " + floor.getFloorNumber());
             }
         }
 
--- src/test/java/com/challenge/kindalab/elevator_system/controllers/ElevatorControllerTest.java
+++ src/test/java/com/challenge/kindalab/elevator_system/controllers/ElevatorControllerTest.java
@@ -4,21 +4,26 @@ import com.challenge.kindalab.elevator_system.domain.Building;
 import com.challenge.kindalab.elevator_system.domain.Elevator;
 import com.challenge.kindalab.elevator_system.domain.Floor;
 import com.challenge.kindalab.elevator_system.handlers.ElevatorRequestHandler;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Collections;
 import java.util.TreeMap;
+import java.util.UUID;
 
 import static com.challenge.kindalab.elevator_system.domain.Elevator.ElevatorBuilder;
 import static com.challenge.kindalab.elevator_system.domain.Elevator.Type.PUBLIC;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,8 +32,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ElevatorController.class)
 public class ElevatorControllerTest {
 
-    private final ElevatorBuilder elevatorBuilder = Elevator.builder().type(PUBLIC);
+    private static final int ANY_WEIGHT = 123;
+    private static final int GROUND_FLOOR_NUMBER = 0;
+
     private final Floor floor3 = new Floor(3, null);
+    private final Elevator.Cabin.CabinBuilder cabinBuilder = Elevator.Cabin.builder().floorNumber(GROUND_FLOOR_NUMBER).weight(ANY_WEIGHT);
+    private final ElevatorBuilder elevatorBuilder = Elevator.builder().id(0).type(PUBLIC).cabin(cabinBuilder.build());
 
     @Autowired
     private MockMvc mockMvc;
@@ -39,9 +48,15 @@ public class ElevatorControllerTest {
     @MockitoBean
     private ElevatorRequestHandler mockRequestHandler;
 
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(mockBuilding, mockRequestHandler);
+    }
+
     @Test
     void getElevators_shouldReturnAListOfElevators() throws Exception {
         given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevatorBuilder.build()));
+
         mockMvc.perform(get("/elevators"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
@@ -50,11 +65,182 @@ public class ElevatorControllerTest {
     }
 
     @Test
-    void callElevator_shouldAddAnElevatorRequest() throws Exception {
-        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevatorBuilder.build()));
+    void call_whenIdNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.emptyList());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/call/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Elevator not found, please check provided id: %s", elevator.getId());
+        verify(mockBuilding).getElevators();
+    }
+
+    @Test
+    void call_whenFloorNumberNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.emptyMap()));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/call/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Floor not found, please check provided floorNumber: %s", floor3.getFloorNumber());
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+    }
+
+    @Test
+    void call_shouldAddAnElevatorRequest() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
         given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
-        mockMvc.perform(put("/elevators/{id}/call/{floorNumber}", 0, floor3.getFloorNumber()))
-                .andExpect(status().isOk());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/call/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isOk()).andReturn();
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+        verify(mockRequestHandler).call(elevator, floor3);
+    }
+
+    @Test
+    void load_whenIdNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.emptyList());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/load/{floorNumber}?weight={weight}", elevator.getId(), floor3.getFloorNumber(), ANY_WEIGHT))
+                .andExpect(status().isNotFound()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Elevator not found, please check provided id: %s", elevator.getId());
+        verify(mockBuilding).getElevators();
+    }
+
+
+    @Test
+    void load_whenFloorNumberNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.emptyMap()));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/load/{floorNumber}?weight={weight}", elevator.getId(), floor3.getFloorNumber(), ANY_WEIGHT))
+                .andExpect(status().isNotFound()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Floor not found, please check provided floorNumber: %s", floor3.getFloorNumber());
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+    }
+
+    @Test
+    void load_whenElevatorCabinIsOnADifferentFloor_shouldThrowConflictException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/load/{floorNumber}?weight={weight}", elevator.getId(), floor3.getFloorNumber(), ANY_WEIGHT))
+                .andExpect(status().isConflict()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Elevator Cabin isn't on this floor yet, please use floorNumber: %s", cabinBuilder.build().getFloorNumber());
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+    }
+
+    @Test
+    void move_whenIdNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.emptyList());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/move", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isNotFound()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Elevator not found, please check provided id: %s", elevator.getId());
+        verify(mockBuilding).getElevators();
+    }
+
+    @Test
+    void move_whenRequestIsEmpty_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/move", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isConflict()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("There are not pending requests for Elevator, you can add request by using elevator call or elevator destination APIs");
+        verify(mockBuilding).getElevators();
+    }
+
+    @Test
+    void destination_whenIdNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.emptyList());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isNotFound()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Elevator not found, please check provided id: %s", elevator.getId());
+        verify(mockBuilding).getElevators();
+    }
+
+    @Test
+    void destination_whenFloorNumberNotExist_shouldThrowNotFoundException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.emptyMap()));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isNotFound()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Floor not found, please check provided floorNumber: %s", floor3.getFloorNumber());
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+    }
+
+    @Test
+    void destination_whenIsFreightElevator_shouldCallRequestHandler() throws Exception {
+        Elevator elevator = elevatorBuilder.type(Elevator.Type.FREIGHT).build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
+        doNothing().when(mockRequestHandler).call(elevator, floor3);
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isOk()).andReturn();
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+        verify(mockRequestHandler).call(elevator, floor3);
+    }
+
+    @Test
+    void destination_whenFloorKeycardIsNull_shouldCallRequestHandler() throws Exception {
+        Elevator elevator = elevatorBuilder.type(Elevator.Type.FREIGHT).build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
+        doNothing().when(mockRequestHandler).call(elevator, floor3);
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isOk()).andReturn();
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+        verify(mockRequestHandler).call(elevator, floor3);
+    }
+
+    @Test
+    void destination_whenRequiredKeycardNotProvided_shouldThrownException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        floor3.setKeycard(UUID.randomUUID().toString());
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}", elevator.getId(), floor3.getFloorNumber()))
+                .andExpect(status().isConflict()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Keycard is required to reach the floor: %s", floor3.getFloorNumber());
+        verify(mockBuilding).getElevators();
+        verify(mockBuilding).getFloors();
+    }
+
+    @Test
+    void destination2_whenNotCorrectKeyCardIsProvided_shouldThrownException() throws Exception {
+        Elevator elevator = elevatorBuilder.build();
+        given(mockBuilding.getElevators()).willReturn(Collections.singletonList(elevator));
+        floor3.setKeycard(UUID.randomUUID().toString());
+        given(mockBuilding.getFloors()).willReturn(new TreeMap<>(Collections.singletonMap(floor3.getFloorNumber(), floor3)));
+        UUID provided = UUID.randomUUID();
+        assumeThat(floor3.getKeycard()).isNotEqualTo(provided.toString());
+
+        MvcResult actual = mockMvc.perform(put("/elevators/{id}/destination/{floorNumber}?keycard={keycard}", elevator.getId(), floor3.getFloorNumber(), provided.toString()))
+                .andExpect(status().isForbidden()).andReturn();
+        assertThat(actual.getResponse().getErrorMessage()).isEqualTo("Provided keycard does match, please check keycard for floorNumber: %s", floor3.getFloorNumber());
         verify(mockBuilding).getElevators();
         verify(mockBuilding).getFloors();
     }
